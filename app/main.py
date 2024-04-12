fdf7096f42bf472005dff5071a83fadc4987a7cd
qlstn <ghkdtnqls1123@naver.com>
2024-04-12 17:13:51 +0900

    commit

--- app/main.py
+++ app/main.py
@@ -5,21 +5,20 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-name = "subin"
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
  
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/")
-def read_root():
-    return {"Hello": "World"}
 
-@app.update("/")
-def update_root():
-    return {"Hello": "World"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
-@app.delete("/")
-def delete_root():
-    return {"Hello": "World"}
-
+@app.post("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id"}
