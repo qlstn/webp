b173979cbe3c397044e1ac411a60e81ce17f45fc
qlstn <ghkdtnqls1123@naver.com>
2024-04-19 15:50:53 +0900

    commit

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
+import requests
+
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return { "message": "Hello Root!" }
+    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&bizCd=C&apiKey=VWICH9DPhFOx1x992yQ09VW3jf2jw0mi7P9h96s4&returnType=json"
+    
+    contents = requests.get(URL).text
+    
+    return { "message"" contents }
 
 @app.get("/home")
 def home():
